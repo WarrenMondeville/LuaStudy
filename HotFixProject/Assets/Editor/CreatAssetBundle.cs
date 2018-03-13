f28e6a40cd82e355d7b9cdeb25c6df7c806a7263
WarrenMondeville <541008505@qq.com>
2018-03-13 19:19:49 +0800

    assetbundle

---
+++ HotFixProject/Assets/Editor/CreatAssetBundle.cs
@@ -0,0 +1,14 @@
+﻿
+
+using UnityEditor;
+
+public class CreateAssetBundle {
+
+    [MenuItem("Assets/Build/AssetBundles")]
+    static int BuildAllAssetsBundles()
+    {
+
+        BuildPipeline.BuildAssetBundles("Assets/StreamingAssets/ABs", BuildAssetBundleOptions.None, BuildTarget.StandaloneWindows);
+        return 0;
+    }
+}
---
+++ HotFixProject/Assets/Scripts/LoadAssetBoundle.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.IO;
+
+public class LoadAssetBoundle : MonoBehaviour
+{
+
+    public string URL;
+    public string assetname;
+    IEnumerator Start()
+    {
+        // string url = Path.Combine("file://F:/MyGit/LuaStudy/HotFixProject/Assets/StreamingAssets"/*+Application.streamingAssetsPath*/, URL);
+        string url = Path.Combine("file://"+Application.streamingAssetsPath.Replace("'\'", "'/'"), URL);
+        Debug.Log(url);
+        using (WWW www = new WWW( url))
+        {
+            yield return www;
+            if (www.error != null)
+            {
+                Debug.Log("net error" + www.error);
+            }
+            else
+            {
+                AssetBundle bundle = www.assetBundle;
+                var obj = bundle.LoadAsset<GameObject>(assetname);
+                Instantiate<GameObject>(obj);
+                bundle.Unload(false);
+            }
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Application.streamingAssetsPath.Replace("'\'","'/'");
+    }
+}
